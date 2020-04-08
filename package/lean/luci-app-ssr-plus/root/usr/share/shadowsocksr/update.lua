927b5f7b298c31c4ec96373f857838a85ffeb629
eyeopen <lifuyi@hotmail.com>
2020-04-08 11:03:29 +0800

    to april 1

--- package/lean/luci-app-ssr-plus/root/usr/share/shadowsocksr/update.lua
+++ package/lean/luci-app-ssr-plus/root/usr/share/shadowsocksr/update.lua
@@ -21,12 +21,17 @@ end
 
 log('正在更新【GFW列表】数据库')
 <<<<<<< HEAD
+<<<<<<< HEAD
 refresh_cmd = "wget-ssl --no-check-certificate -O- " .. uci:get_first('shadowsocksr', 'global', 'gfwlist_url', 'https://cdn.jsdelivr.net/gh/gfwlist/gfwlist/gfwlist.txt') .. " > /tmp/gfw.b64"
+=======
+refresh_cmd = "wget-ssl --no-check-certificate -O - " .. uci:get_first('shadowsocksr', 'global', 'gfwlist_url', 'https://cdn.jsdelivr.net/gh/gfwlist/gfwlist/gfwlist.txt') .. " > /tmp/gfw.b64"
+>>>>>>> parent of ac00994abf... Merge branch 'master' of https://github.com/coolsnowwolf/lede into dev-lean-lede
 sret = luci.sys.call(refresh_cmd .. " 2>/dev/null")
 if sret == 0 then
 	luci.sys.call("/usr/bin/ssr-gfw")
 	icount = luci.sys.exec("cat /tmp/gfwnew.txt | wc -l")
 	if tonumber(icount) > 1000 then
+<<<<<<< HEAD
 		if nixio.fs.access("/etc/dnsmasq.ssr/gfw_list.conf") then
 			oldcount = luci.sys.exec("cat /etc/dnsmasq.ssr/gfw_list.conf | wc -l")
 		else
@@ -41,10 +46,13 @@ if sret == 0 then
 	if tonumber(icount)>1000 then
 	oldcount=luci.sys.exec("cat /etc/dnsmasq.ssr/gfw_list.conf | wc -l")
 >>>>>>> parent of 0382a499c2... luci-app-ssr-plus:Adjust custom item display (#3879)
+=======
+		oldcount = luci.sys.exec("cat /etc/dnsmasq.ssr/gfw_list.conf | wc -l")
+>>>>>>> parent of ac00994abf... Merge branch 'master' of https://github.com/coolsnowwolf/lede into dev-lean-lede
 		if tonumber(icount) ~= tonumber(oldcount) then
 			luci.sys.exec("cp -f /tmp/gfwnew.txt /etc/dnsmasq.ssr/gfw_list.conf")
 			luci.sys.exec("cp -f /tmp/gfwnew.txt /tmp/dnsmasq.ssr/gfw_list.conf")
-			log('更新成功！ 新的总纪录数：' .. tostring(tonumber(icount)/2))
+			log('更新成功！ 新的总纪录数：'.. tostring(tonumber(icount)/2))
 		else
 			log('你已经是最新数据，无需更新！')
 		end
@@ -58,7 +66,11 @@ end
 
 log('正在更新【国内IP段】数据库')
 <<<<<<< HEAD
+<<<<<<< HEAD
 refresh_cmd = "wget-ssl --no-check-certificate -O- " .. uci:get_first('shadowsocksr', 'global', 'chnroute_url','https://ispip.clang.cn/all_cn.txt') .. " > /tmp/china_ssr.txt"
+=======
+refresh_cmd = "wget-ssl --no-check-certificate -O - ".. uci:get_first('shadowsocksr', 'global', 'chnroute_url','https://ispip.clang.cn/all_cn.txt') .." > /tmp/china_ssr.txt"
+>>>>>>> parent of ac00994abf... Merge branch 'master' of https://github.com/coolsnowwolf/lede into dev-lean-lede
 sret = luci.sys.call(refresh_cmd .. " 2>/dev/null")
 =======
 if (ucic:get_first('shadowsocksr', 'global', 'chnroute','0') == '1' ) then
@@ -73,6 +85,7 @@ if sret== 0 then
 	icount = luci.sys.exec("cat /tmp/china_ssr.txt | wc -l")
 <<<<<<< HEAD
 	if tonumber(icount) > 1000 then
+<<<<<<< HEAD
 		if nixio.fs.access("/etc/china_ssr.txt") then
 			oldcount = luci.sys.exec("cat /etc/china_ssr.txt | wc -l")
 		else
@@ -82,9 +95,12 @@ if sret== 0 then
 	if tonumber(icount)>1000 then
 		oldcount=luci.sys.exec("cat /etc/china_ssr.txt | wc -l")
 >>>>>>> parent of 0382a499c2... luci-app-ssr-plus:Adjust custom item display (#3879)
+=======
+		oldcount = luci.sys.exec("cat /etc/china_ssr.txt | wc -l")
+>>>>>>> parent of ac00994abf... Merge branch 'master' of https://github.com/coolsnowwolf/lede into dev-lean-lede
 		if tonumber(icount) ~= tonumber(oldcount) then
 			luci.sys.exec("cp -f /tmp/china_ssr.txt /etc/china_ssr.txt")
-			log('更新成功！ 新的总纪录数：' .. tostring(tonumber(icount)))
+			log('更新成功！ 新的总纪录数：'.. icount)
 		else
 			log('你已经是最新数据，无需更新！')
 		end
@@ -99,7 +115,7 @@ end
 <<<<<<< HEAD
 if uci:get_first('shadowsocksr', 'global', 'adblock','0') == "1" then
 	log('正在更新【广告屏蔽】数据库')
-	refresh_cmd = "wget-ssl --no-check-certificate -O- " .. uci:get_first('shadowsocksr', 'global', 'adblock_url','https://easylist-downloads.adblockplus.org/easylistchina+easylist.txt') .. " > /tmp/adnew.conf"
+	refresh_cmd = "wget-ssl --no-check-certificate -O - ".. uci:get_first('shadowsocksr', 'global', 'adblock_url','https://easylist-downloads.adblockplus.org/easylistchina+easylist.txt') .." > /tmp/adnew.conf"
 	sret = luci.sys.call(refresh_cmd .. " 2>/dev/null")
 	if sret == 0 then
 		luci.sys.call("/usr/bin/ssr-ad")
@@ -113,7 +129,7 @@ if uci:get_first('shadowsocksr', 'global', 'adblock','0') == "1" then
 			if tonumber(icount) ~= tonumber(oldcount) then
 				luci.sys.exec("cp -f /tmp/ad.conf /etc/dnsmasq.ssr/ad.conf")
 				luci.sys.exec("cp -f /tmp/ad.conf /tmp/dnsmasq.ssr/ad.conf")
-				log('更新成功！ 新的总纪录数：' .. tostring(tonumber(icount)))
+				log('更新成功！ 新的总纪录数：'.. icount)
 			else
 				log('你已经是最新数据，无需更新！')
 			end
@@ -143,20 +159,16 @@ end
 
 --[[
 log('正在更新【Netflix IP段】数据库')
-refresh_cmd = "wget-ssl --no-check-certificate -O- " .. uci:get_first('shadowsocksr', 'global', 'nfip_url','https://raw.githubusercontent.com/QiuSimons/Netflix_IP/master/NF_only.txt') .. " > /tmp/netflixip.list"
+refresh_cmd = "wget-ssl --no-check-certificate -O - ".. uci:get_first('shadowsocksr', 'global', 'nfip_url','https://raw.githubusercontent.com/QiuSimons/Netflix_IP/master/NF_only.txt') .." > /tmp/netflixip.list"
 sret = luci.sys.call(refresh_cmd .. " 2>/dev/null")
 if sret == 0 then
 	luci.sys.call("/usr/bin/ssr-gfw")
 	icount = luci.sys.exec("cat /tmp/netflixip.list | wc -l")
 	if tonumber(icount) > 5 then
-		if nixio.fs.access("/etc/config/netflixip.list") then
-			oldcount = luci.sys.exec("cat /etc/config/netflixip.list | wc -l")
-		else
-			oldcount = "0"
-		end
+		oldcount = luci.sys.exec("cat /etc/config/netflixip.list | wc -l")
 		if tonumber(icount) ~= tonumber(oldcount) then
 			luci.sys.exec("cp -f /tmp/netflixip.list /etc/config/netflixip.list")
-			log('更新成功！ 新的总纪录数：' .. tostring(tonumber(icount)))
+			log('更新成功！ 新的总纪录数：'.. icount)
 		else
 			log('你已经是最新数据，无需更新！')
 		end
@@ -178,8 +190,13 @@ else
 end
 <<<<<<< HEAD
 --]]
+<<<<<<< HEAD
 =======
 end
 
 luci.sys.call("/etc/init.d/dnsmasq reload")
 >>>>>>> parent of 0382a499c2... luci-app-ssr-plus:Adjust custom item display (#3879)
+=======
+
+luci.sys.call("/etc/init.d/dnsmasq reload")
+>>>>>>> parent of ac00994abf... Merge branch 'master' of https://github.com/coolsnowwolf/lede into dev-lean-lede
